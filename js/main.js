aa182abefee57a9e804fe5f4668b1204af9019cb
George <george.heming.03@gmail.com>
2022-09-17 15:54:55 +0500

    Исправил по линту

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 function getRandomIntInclusive(min, max) {
-    if (min < 0 || max < min){
-        console.log("Введены некорректные значения диапазона");
-        return;
-    }
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  if (min < 0 || max < min){
+    console.log('Введены некорректные значения диапазона');
+    return;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-  /*
+/*
     Взято с сайта MDN: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#parameters
   */
 function isLessThanMax(string, maxLength){
-    return string.Length < maxLength ? true : false;
+  return string.Length < maxLength;
 }
