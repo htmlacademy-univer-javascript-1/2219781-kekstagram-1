19eee60be96ff6fc1595561f2e904c78f2730533
George <george.heming.03@gmail.com>
2022-09-20 20:09:29 +0500

    Переделал по стилю

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
+// Взято с сайта MDN: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#parameters
 function getRandomIntInclusive(min, max) {
-  if (min < 0 || max < min){
-    return('Введены некорректные значения диапазона');
+  if (min < 0 || max < min) {
+    throw new SyntaxError('Введены некорректные значения диапазона');
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-/*
-    Взято с сайта MDN: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#parameters
-*/
-function isLessThanMax(string, maxLength){
-  return string.length < maxLength;
+
+function isLessThanMax(string, maxLength) {
+  return string.length <= maxLength;
 }
 
-getRandomIntInclusive(1,10);
+getRandomIntInclusive(1, 10);
 isLessThanMax('Hello,world', 140);
