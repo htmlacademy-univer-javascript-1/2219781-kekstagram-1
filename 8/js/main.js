68671c78cdf009e2bd4ac221e2fa46f0ec5c766d
Keks <keks@htmlacademy.ru>
2022-10-08 16:40:13 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-vars
+import { isLessThanMax } from './util';
+// eslint-disable-next-line no-unused-vars
+import { getRandomIntInclusive } from './data';
---
+++ 8/js/util.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+function isLessThanMax(string, maxLength) {
+  return string.length <= maxLength;
+}
+
+export {isLessThanMax};
